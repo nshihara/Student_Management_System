3bded523cd4a3fbf97aad9bbe98a185f999ecb53
nshihara <nshihara99@gmail.com>
2023-10-10 15:22:36 +0530

    Add styles

--- student-management-system/src/components/StudentTable.js
+++ student-management-system/src/components/StudentTable.js
@@ -1,6 +1,8 @@
 import { Table, Space, Button, Modal, Input, message, Alert, Form, DatePicker, Radio, InputNumber } from "antd";
 import TextArea from "antd/lib/input/TextArea";
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 
 // const { TextArea } = Input; 
 const { RangePicker } = DatePicker;
@@ -27,26 +29,28 @@ const StudentTable = () => {
       title: "Gender",
       dataIndex: "gender",
     },
-    {
-      title: "Action",
-      key: "action",
-      render: (_, record) => (
-        <Space size="middle">
-          <Button type="default" onClick={() => handleView(record)}>
-            View
-          </Button>
-          <Button type="default" onClick={() => handleEdit(record)}>
-            Edit
-          </Button>
-          <Button type="dashed" onClick={() => handleDelete(record)}>
-            Delete
-          </Button>
-        </Space>
-      ),
-      align: "center",
-    },
+    // {
+    //   title: "Action",
+    //   key: "action",
+    //   render: (_, record) => (
+    //     <Space size="middle">
+    //       <Button type="default" onClick={() => handleView(record)}>
+    //         View
+    //       </Button>
+    //       <Button type="default" onClick={() => handleEdit(record)}>
+    //         Edit
+    //       </Button>
+    //       <Button type="dashed" onClick={() => handleDelete(record)}>
+    //         Delete
+    //       </Button>
+    //     </Space>
+    //   ),
+    //   align: "center",
+    // },
   ]);
 
+  const [editedStudent, setEditedStudent] = useState(null); // Add this line
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [dataSource, setDataSource] = useState([]);
   const [isViewModalOpen, setIsViewModalOpen] = useState(false);
   const [isAddModalOpen, setIsAddModalOpen] = useState(false); // State for Add Student modal
@@ -62,11 +66,46 @@ const StudentTable = () => {
   };
 
   const handleEdit = (record) => {
-    console.log(`Editing student with ID ${record.id}`);
+    setEditedStudent(record);
+    setIsEditModalOpen(true);
+    form.setFieldsValue(record); // Populate the form fields with the selected student's data
   };
 
-  const handleDelete = (record) => {
-    console.log(`Deleting student with ID ${record.id}`);
+  const onEditFinish = (values) => {
+    console.log("Edited values:", values);
+
+    // Send a PUT request to update the student's data on the server
+    axios.put(`http://localhost:3000/student/${editedStudent.id}`, values)
+      .then((response) => {
+        console.log("Student edited successfully:", response.data);
+
+        // Update the dataSource with the edited student data
+        const updatedDataSource = dataSource.map((student) => {
+          if (student.id === editedStudent.id) {
+            return { ...student, ...values };
+          }
+          return student;
+        });
+
+        setDataSource(updatedDataSource);
+        setIsEditModalOpen(false);
+        form.resetFields();
+      })
+      .catch((error) => {
+        console.error("Error editing student:", error);
+      });
+  };
+
+  const openEditModal = (record) => {
+    setEditedStudent(record);
+    setIsEditModalOpen(true);
+    form.setFieldsValue(record); // Populate the form fields with the selected student's data
+  };
+  
+  // Function to close the Edit Student modal
+  const closeEditModal = () => {
+    setIsEditModalOpen(false);
+    form.resetFields(); // Reset the form fields when closing the modal
   };
 
   const handleModalCancel = () => {
@@ -120,8 +159,41 @@ const StudentTable = () => {
 
   const onFinish = (values) => {
     console.log("Received values:", values);
-    setIsAddModalOpen(false); // Close Add Student modal after submitting the form
-    form.resetFields();
+  
+    // Create a new student object from the form values
+    const newStudent = {
+      firstName: values.firstName,
+      lastName: values.lastName,
+      gender: values.gender,
+      mobileNumber: values.mobileNumber,
+      birthday: values.birthday.format("YYYY-MM-DD"),
+      address: values.address,
+      guardianName: values.guardianName,
+      guardianContactNumber: values.guardianContactNumber,
+      otherDetails: values.otherDetails,
+    };
+  
+    // Send a POST request to your API endpoint
+    axios.post("http://localhost:3000/student", newStudent)
+      .then((response) => {
+        console.log("Student added successfully:", response.data);
+  
+        // Update the dataSource with the new student data
+        const updatedDataSource = [...dataSource, response.data];
+        setDataSource(updatedDataSource);
+  
+        setIsAddModalOpen(false); // Close Add Student modal after submitting the form
+        form.resetFields();
+      })
+      .catch((error) => {
+        console.error("Error adding student:", error);
+      });
+  };
+  
+  const handleDelete = (record) => {
+    console.log("Delete button clicked for student ID:", record.id);
+    const updatedDataSource = dataSource.filter((student) => student.id !== record.id);
+    setDataSource(updatedDataSource);
   };
 
   return (
@@ -159,10 +231,31 @@ const StudentTable = () => {
         />
       )}
       <Table
-        columns={columns}
-        dataSource={filteredData.length > 0 ? filteredData : dataSource}
-        style={{ margin: "16px" }}
-      ></Table>
+  columns={[
+    ...columns,
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Space size="middle">
+          <Button type="default" onClick={() => handleView(record)}>
+            View
+          </Button>
+          <Button type="default" onClick={() => openEditModal(record)}>
+            Edit
+          </Button>
+          <Button type="dashed" onClick={() => handleDelete(record)}>
+            Delete
+          </Button>
+        </Space>
+      ),
+      align: "center",
+    },
+  ]}
+  dataSource={filteredData.length > 0 ? filteredData : dataSource}
+  style={{ marginTop: "40px", marginLeft: "10px", marginRight: "10px" }}
+  rowKey="id"
+/>
       <Modal
         title="Student Details"
         visible={isViewModalOpen}
@@ -203,6 +296,7 @@ const StudentTable = () => {
           onFinish={onFinish}
           className="custom-form"
         >
+         
           <Form.Item name="firstName" label="First Name">
             <Input className="custom-input"/>
           </Form.Item>
@@ -219,7 +313,7 @@ const StudentTable = () => {
             <InputNumber className="custom-input"/>
           </Form.Item>
           <Form.Item name="birthday" label="Birthday">
-            <DatePicker />
+            <DatePicker format="YYYY-MM-DD" />
           </Form.Item>
           <Form.Item name="address" label="Address">
             <Input className="custom-input"/>
@@ -239,6 +333,56 @@ const StudentTable = () => {
             </Button>
           </Form.Item>
 
+        </Form>
+      </Modal>
+      <Modal
+        title="Edit Student"
+        visible={isEditModalOpen}
+        onCancel={closeEditModal} // Close the Edit Student modal
+        footer={null}
+      >
+        <Form
+          form={form}
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 16 }}
+          onFinish={onEditFinish}
+          className="custom-form"
+        >
+          <Form.Item name="firstName" label="First Name">
+            <Input className="custom-input" />
+          </Form.Item>
+          <Form.Item name="lastName" label="Last Name">
+            <Input className="custom-input" />
+          </Form.Item>
+          <Form.Item name="gender" label="Gender">
+            <Radio.Group>
+              <Radio value="Male">Male</Radio>
+              <Radio value="Female">Female</Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item name="mobileNumber" label="Mobile number">
+            <InputNumber className="custom-input" />
+          </Form.Item>
+          {/* <Form.Item name="birthday" label="Birthday">
+            <DatePicker format="YYYY-MM-DD" />
+          </Form.Item> */}
+          <Form.Item name="address" label="Address">
+            <Input className="custom-input" />
+          </Form.Item>
+          <Form.Item name="guardianName" label="Guardian Name">
+            <Input className="custom-input" />
+          </Form.Item>
+          <Form.Item name="guardianContactNumber" label="Guardian contact number">
+            <InputNumber className="custom-input" />
+          </Form.Item>
+          <Form.Item name="otherDetails" label="Other Details">
+            <TextArea rows={4} className="custom-input" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="custom-button">
+              Save
+            </Button>
+          </Form.Item>
         </Form>
       </Modal>
     </div>
